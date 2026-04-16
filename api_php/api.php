95c250c36825cb549549e3a2ff9730eb21fd2a4b
gmorettirib <guimorettir@gmail.com>
2026-04-16 10:16:10 -0300

    16/04

--- api_php/api.php
+++ api_php/api.php
@@ -4,15 +4,41 @@
 
     $metodo = $_SERVER['REQUEST_METHOD'];
 
-    // ECHO "Método da requisição: ". $metodo;
+    
+    
+    // $usuarios = [
+    //     ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+    //     ["id" => 2, "nome" => "Joao Silva", "email" => "joao@email.com"]
+    // ];
+    // recupera o arquivo json
+    $arquivo = 'usuarios.json';
 
+    //verifica se o arquivo, se nao criar um array vazio
+    if (!file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    $usuarios = json_decode(file_get_contents($arquivo), true);
+    
     switch ($metodo) {
         case 'GET':
-            echo "ações do get";
+            // echo "ações do get";
+            echo json_encode($usuarios);
             break;
 
         case 'POST':
-            echo "ações do post";
+            // echo "ações do post";
+            $dados = json_decode(file_get_contents('php://input'), true);
+            // print_r($dados);
+            $novoUsuario = [
+                "id" => $dados["id"],
+                "nome" => $dados["nome"],
+                "email" => $dados["email"]
+            ];
+            // Adiciona o novo usuario na array existente
+            array_push($usuarios, $novoUsuario);
+            echo json_encode('Usuario inserido com sucesso!');
+            print_r($usuarios);
             break;
         
         default:
@@ -20,11 +46,5 @@
             break;
     }
 
-    // $usuarios = [
-    //     ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-    //     ["id" => 2, "nome" => "Joao Silva", "email" => "joao@email.com"]
-    // ];
-
-    // echo json_encode($usuarios);
 
 ?>
