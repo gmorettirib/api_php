1bc27b6f809d2940afbe86b461e6cfd6b0bc89ab
gmorettirib <guimorettir@gmail.com>
2026-04-23 09:23:07 -0300

    23-04

--- api_php/api.php
+++ api_php/api.php
@@ -23,26 +23,39 @@
     switch ($metodo) {
         case 'GET':
             // echo "ações do get";
-            echo json_encode($usuarios);
+            echo json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
             break;
 
         case 'POST':
             // echo "ações do post";
             $dados = json_decode(file_get_contents('php://input'), true);
             // print_r($dados);
-            $novoUsuario = [
+
+            if (!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"])){
+                http_response_code(400);             
+                echo json_encode(["erro" => "Dados incompletos. "], JSON_UNESCAPED_UNICODE);
+                exit;
+            }
+            $novo_usuario = [
                 "id" => $dados["id"],
                 "nome" => $dados["nome"],
                 "email" => $dados["email"]
             ];
-            // Adiciona o novo usuario na array existente
-            array_push($usuarios, $novoUsuario);
-            echo json_encode('Usuario inserido com sucesso!');
-            print_r($usuarios);
+
+            $usuarios[] = $novo_usuario;
+            file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+
+            echo json_encode(["mensagem" => "Usuário inserido com sucesso!", "usuarios" => $usuarios], JSON_UNESCAPED_UNICODE);
+            // // Adiciona o novo usuario na array existente
+            // array_push($usuarios, $novoUsuario);
+            // echo json_encode('Usuario inserido com sucesso!');
+            // print_r($usuarios);
             break;
         
         default:
-            echo "método não encontrado";
+            // echo "método não encontrado";
+            http_response_code(405);
+            echo json_encode(["erro" => "Metodo não permitido!"], JSON_UNESCAPED_UNICODE);
             break;
     }
 
